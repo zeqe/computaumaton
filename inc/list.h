a9974233447dbb7a937652c55a8bc139c83fb4ac
zeqe <zeque@live.unc.edu>
2022-12-17 02:52:07 -0500

    Wrote list declaration as a macro and documented it

---
+++ inc/list.h
@@ -0,0 +1,85 @@
+#ifndef LIST_INCLUDED
+	#include "uint.h"
+	
+	#define LIST_BLOCK_LEN 256
+	
+	/*
+	list:
+	
+	ILLUSTRATION-----------------------------||
+	
+	|                     [---i=1---]            |len=3
+	|                     \         /            |
+	| block: [(#,#,#,#,#),(#,#,#,#,#),(#,#,#,#,#),(_,_,_,_,_),(_,_,_,_,_),...]
+	
+	Each value '#' is of type T.
+	In this example, N = 5.
+	
+	'i' is the current position in the list.
+	'block' is the statically allocated space for the list to hold its items; it has enough space to
+	contain LIST_BLOCK_LEN N-tuples.
+	
+	Thus:
+	  0 <= i <  len      <= LIST_BLOCK_LEN
+	Unless len == 0, then:
+	  0 <= i <= len == 0 <= LIST_BLOCK_LEN
+	
+	NOTES -----------------------------------||
+	
+	NAME determines the name of the resultant list struct, and PREFIX determines the prefix prepended
+	to the generated functions to uniquely discern them from those corresponding to other list types.
+	
+	Documentation for the iterative functions:
+	  _forall:   f is called as f(T val[N],uint index)
+	  
+	  _removeif: f is called as f(T val[N])
+	             if f returns 0, the element will be kept.
+	             if f returns != 0, the element will be removed.
+	             
+	  _contains: will return 1 if the list contains val, 0 otherwise
+	*/
+	
+	#define LIST_EMPTY {0,0}
+	
+	#define LIST_DECL(T,N,NAME,PREFIX)\
+		struct NAME {\
+			uint len;                   \
+			uint i;                     \
+			\
+			T block[N * LIST_BLOCK_LEN];\
+		};\
+		\
+		uint PREFIX ## _len(struct NAME *l);\
+		uint PREFIX ## _i  (struct NAME *l);\
+		\
+		void PREFIX ## _add_after(struct NAME *l,T val[N]);\
+		void PREFIX ## _remove   (struct NAME *l);         \
+		\
+		void PREFIX ## _val_forward (struct NAME *l);\
+		void PREFIX ## _val_backward(struct NAME *l);\
+		\
+		void PREFIX ## _i_forward (struct NAME *l);\
+		void PREFIX ## _i_backward(struct NAME *l);\
+		\
+		void PREFIX ## _get(struct NAME *l,T val[N]);\
+		void PREFIX ## _set(struct NAME *l,T val[N]);\
+		\
+		void PREFIX ## _forall  (struct NAME *l,void (*f)(T [N],uint));\
+		void PREFIX ## _removeif(struct NAME *l,uint (*f)(T [N]));     \
+		uint PREFIX ## _contains(struct NAME *l,T val[N]);
+	
+	// Generic uint-tuple lists
+	LIST_DECL(uint,1,list_uint1,lu1)
+	LIST_DECL(uint,3,list_uint3,lu3)
+	LIST_DECL(uint,5,list_uint5,lu5)
+	
+	// Symbols tie-in: a specialized list specifying the maximum number of bytes per symbol-element
+	#include "symbol.h"
+	
+	struct list_sym{
+		uint sym_max_bytes;
+		struct list_uint1 sym_list;
+	};
+	
+	#define LIST_INCLUDED
+#endif
