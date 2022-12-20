2b6c1a7ed27af541320251cec01d60a102e3973d
zeqe <zeque@live.unc.edu>
2022-12-19 23:16:32 -0500

    Documented set(N)

--- inc/set.h
+++ inc/set.h
@@ -4,26 +4,36 @@
 	#include "symread.h"
 	
 	/*
-	set(N): a list of symbol-tuples with its own symread(N) state
+	set(N): a list of unique symbol N-tuples with its own symread(N) state
 	
-	set(N)_update() handles navigation and modification of the list.
-	Any element added to the set will be checked for uniqueness; if it is not unique, it will be
-	rejected.
+	_update():
+	- handles navigation and modification of the list as dictated by a sequence of input characters
+	- ensures that elements added to the list are unique (duplicates will be rejected)
+	- will return 0 if the operation performed is incomplete, or 1 if it is complete
+	    ...
+	    _update() -> 1 // first operation complete 
+	    _update() -> 0 // second operation not complete
+	    _update() -> 0 // still not complete
+	    _update() -> 0 // ...
+	    _update() -> 1 // second operation complete!
+	    _update() -> 1 // third operation complete
+	    _update() -> 1 // fourth operation complete
+	    ...
 	*/
 	
 	#define set(N) set ## N
 	
 	#define SET_DECL(N)\
 		struct set(N){\
-			struct symread(N) read;  \
 			uint is_reading;         \
+			struct symread(N) read;  \
 			\
 			struct list_uint(N) list;\
 		};\
 		\
 		uint set(N) ## _update(struct set(N) *set,int in);
 	
-	#define SET_INIT(MAX_BYTES,SRCS) {SYMREAD_INIT(MAX_BYTES,SRCS),0,LIST_INIT}
+	#define SET_INIT(MAX_BYTES,SRCS) {0,SYMREAD_INIT(MAX_BYTES,SRCS),LIST_INIT}
 	
 	SET_DECL(1)
 	SET_DECL(3)
